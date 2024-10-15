e16c467825cff01845354ce530c5a406ab389c72
Jacob Tidiakh Ndiapaly Ndiaye <jacobndiaye0113@gmail.com>
2024-10-15 02:02:40 -1200

    add REACT2

---
+++ my-fifa-app/src/player.js
@@ -0,0 +1,40 @@
+const players = [
+  {
+    name: "Lionel Messi",
+    team: "Paris Saint-Germain",
+    nationality: "Argentinian",
+    jerseyNumber: 30,
+    age: 34,
+    imageUrl:
+      "https://cdn.britannica.com/35/238335-050-2CB2EB8A/Lionel-Messi-Argentina-Netherlands-World-Cup-Qatar-2022.jpg",
+  },
+  {
+    name: "Cristiano Ronaldo",
+    team: "Manchester United",
+    nationality: "Portuguese",
+    jerseyNumber: 7,
+    age: 36,
+    imageUrl:
+      "https://cloudfront-eu-central-1.images.arcpublishing.com/leparisien/V4BDAJX6KVCWHLT67PFSPG4DJ4.jpg",
+  },
+  {
+    name: "Kylian Mbappé",
+    team: "Paris Saint-Germain",
+    nationality: "French",
+    jerseyNumber: 7,
+    age: 22,
+    imageUrl:
+      "https://cdn.britannica.com/39/239139-050-49A950D1/French-soccer-player-Kylian-Mbappe-FIFA-World-Cup-December-10-2022.jpg",
+  },
+  {
+    name: "Neymar Jr",
+    team: "Paris Saint-Germain",
+    nationality: "Brazilian",
+    jerseyNumber: 10,
+    age: 29,
+    imageUrl:
+      "https://cdn.britannica.com/95/238295-050-2DDEF573/Neymar-Brazil-Dejan-Lovren-Croatia-FIFA-World-Cup-action-soccer-December-9-2022.jpg",
+  },
+];
+
+export default players;
